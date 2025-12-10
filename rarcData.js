d607e5388ff4ed4b2c01fc7137765a8a4d0d7797
tej1040 <83955867+tej1040@users.noreply.github.com>
2025-12-11 01:41:22 +0530

    rarcData.js

--- rarcData.js
+++ rarcData.js
@@ -4,7 +4,6 @@ const rarcData = {
     "M2": "Not paid separately when the patient is an inpatient.",
     "M3": "Equipment is the same or similar to equipment already being used.",
     "M4": "Alert: This is the last monthly installment payment for this durable medical equipment.",
-    "Notes:": "To be used with the Medicare Drug Price Negotiation Program only.",
     "M5": "Monthly rental payments can continue until the earlier of the 15th month from the first rental month, or the month when the equipment is no longer needed.",
     "M6": "Alert: You must furnish and service this item for any period of medical need for the remainder of the reasonable useful lifetime of the equipment.",
     "M7": "No rental payments after the item is purchased, returned or after the total of issued rental payments equals the purchase price.",
